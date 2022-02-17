79322af7123e79f6a45ea33ecc99c3fdaae88e8f
Maria Sutanto <maria.w.sutanto@gmail.com>
2022-02-17 15:54:20 +0100

    validation and asc for watch list done

---
+++ app/controllers/lists_controller.rb
@@ -0,0 +1,16 @@
+class ListsController < ApplicationController
+  def index
+    @list = List.all
+  end
+
+  def show
+    @list = List.find(params[:id])
+  end
+
+  def new
+    @list = List.new
+
+  def create
+
+  end
+end
